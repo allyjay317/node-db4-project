edf02189f0c3a46276c27bd3e968e612a95bb235
Allison Usher <allyjay317@gmail.com>
2020-09-24 11:24:12 -0700

    created endpoints

--- index.js
+++ index.js
@@ -1,4 +1,48 @@
 const server = require('./server')
+const db = require('./data/cookbookModel')
+
+server.get('/api/recipes', (req, res) => {
+  db.getRecipes()
+    .then(recipeList => {
+      res.status(200).json(recipeList)
+    })
+    .catch(err => {
+      res.status(500).json({ message: 'Sorry, something went wrong', error: err })
+    })
+})
+
+server.get('/api/recipes/:id/shoppingList', (req, res) => {
+  const { id } = req.params
+  db.getShoppingList(id)
+    .then(shoppingList => {
+      res.status(200).json(shoppingList)
+    })
+    .catch(err => {
+      res.status(500).json({ message: 'Sorry, something went wrong', error: err })
+    })
+})
+
+server.get('/api/recipes/:id/instructions', (req, res) => {
+  const { id } = req.params
+  db.getInstructions(id)
+    .then(instructions => {
+      res.status(200).json(instructions)
+    })
+    .catch(err => {
+      res.status(500).json({ message: 'Sorry, something went wrong', error: err })
+    })
+})
+
+server.get('/api/ingredients/:id/recipes', (req, res) => {
+  const { id } = req.params
+  db.getRecipesWithIngredient(id)
+    .then(recipes => {
+      res.status(200).json(recipes)
+    })
+    .catch(err => {
+      res.status(500).json({ message: 'Sorry, something went wrong', error: err })
+    })
+})
 
 server.listen(4000, () => {
   console.log('listening')
